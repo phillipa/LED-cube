f553a049f0e12f4f2405d0bb5737445113733a86
Matt Brooks <lessthanepsion@gmail.com>
2018-09-21 00:19:33 -0600

    Refactored UDP client - set aside test methods Cleanup of ledwall

--- UDPClient/UDPClient/Program.cs
+++ UDPClient/UDPClient/Program.cs
@@ -5,121 +5,163 @@ using System.Threading;
 
 namespace LEDUDPClient
 {
-
+    public static class CRGBColors
+    {
+        public static Byte[] BLACK = { 0x00, 0x00, 0x00 };
+        public static Byte[] RED = { 0xcc, 0x00, 0x00 };
+        public static Byte[] BLUE = { 0x00, 0x00, 0xcc };
+        public static Byte[] GREEN = { 0x00, 0xcc, 0x00 };
+        public static Byte[] PURPLE = { 0xcc, 0x00, 0xcc };
+        public static Byte[] ORANGE = { 0xff, 0xa5, 0x00 };
+        public static Byte[] AQUA = { 0x00, 0xff, 0xff };
+    }
 
     class MainClass
     {
+        public const string UDP_HOST = "192.168.2.1";
+        public const int UDP_PORT = 7777;
+        public const int FRAME_SIZE = 10;
+        public static readonly Byte[] END = { 0x01, 0x00, 0x01 };   // clear
+        public static readonly Byte[] START = { 0x01, 0x01, 0x00 }; // reset
 
+        public static void LEDTestSequence(UdpClient client)
+        {
+            int delayBetweenTests = 1000;
+
+            // START Test
+            Console.WriteLine("Sending start sequence...");
+            client.Send(START, START.Length);
+            Thread.Sleep(delayBetweenTests);
+
+            // END Test
+            Console.WriteLine("Sending end sequence...");
+            client.Send(END, END.Length);
+            Thread.Sleep(delayBetweenTests);
+
+            // Clear the frame
+            Console.WriteLine("Wipe the whole frame...");
+            Byte[] clearFrame = sendSingleColorFrame(FRAME_SIZE, CRGBColors.BLACK);
+            client.Send(clearFrame, clearFrame.Length);
+            Thread.Sleep(delayBetweenTests);
+
+            // Purple
+            Console.WriteLine("Purple...");
+            Byte[] purples = sendSingleColorFrame(FRAME_SIZE, CRGBColors.PURPLE);
+            client.Send(purples, purples.Length);
+            Thread.Sleep(delayBetweenTests);
+
+            Console.WriteLine("Done...");
+            Console.ReadKey();
+            return;
+        }
 
         public static void Main(string[] args)
         {
-              UdpClient udpClient = new UdpClient();
+            var udpClient = new UdpClient(UDP_HOST, UDP_PORT);
+
             try
-            {
+            {               
+                // TODO: Will use key input for controlling color
+                //String input = Console.ReadLine();
 
-                //  String input = Console.ReadLine();
-                //  while (input != "q")
-                //  {
-
-
-                Byte[] red = { 0xcc, 0x00, 0x00 };
-                Byte[] blue = { 0x00, 0x00, 0xcc };
-                Byte[] green = { 0x00, 0xcc, 0x00 };
-                Byte[] purple = { 0xcc, 0x00, 0xcc };
-                Byte[] orange = { 0xff, 0xa5, 0x00 };
-                Byte[] aqua = { 0x00, 0xff, 0xff };
-
-                Byte[] clearBytes = { 0x01, 0x00, 0x01 };
-                Byte[] rstBytes = { 0x01, 0x01, 0x00 };
-
-                Byte[] sendBytes = { 0xcc, 0x00, 0xcc };
-
-                /*  if (input == "r")
-                      sendBytes = red;
-                  else if (input == "g")
-                      sendBytes = green;
-                  else if (input == "b")
-                      sendBytes = blue;
-                  else
-                      sendBytes = purple;*/
-
-
-               // udpClient.Send(rstBytes, rstBytes.Length, "192.168.0.27", 7777);
-
-                Byte[] tosend = sendSingleColorFrame(300, purple);
-                for (int i = 0; i < (1000*15); i++)
-                {
-                    if(i%3==0)
-                    tosend = sendSinglePixelFrame(15 * 4, i%(15*4), purple);
-                    else if (i%3 ==1)
-                    tosend = sendSinglePixelFrame(15 * 4, i % (15 * 4), orange);
-                    else if(i%3 ==2)
-                    tosend = sendSinglePixelFrame(15 * 4, i % (15 * 4), aqua);
-
-                    udpClient.Send(tosend, tosend.Length, "192.168.0.27", 7777);
-                    Thread.Sleep(20);
-                   
-                }
-                //udpClient.Send(clearBytes, clearBytes.Length, "192.168.0.27", 7777);
-
-
+                Console.WriteLine("Starting LED test sequence...");
+                LEDTestSequence(udpClient);
                 udpClient.Close();
+                return;
+
+                //for (int i = 0; i < (1000 * FRAME_SIZE); i++)
+                //{
+                //    if (i % 3 == 0)
+                //        tosend = sendSinglePixelFrame(15 * 4, i % (15 * 4), purple);
+                //    else if (i % 3 == 1)
+                //        tosend = sendSinglePixelFrame(15 * 4, i % (15 * 4), orange);
+                //    else if (i % 3 == 2)
+                //        tosend = sendSinglePixelFrame(15 * 4, i % (15 * 4), aqua);
+
+                //    udpClient.Send(tosend, tosend.Length, UDP_HOST, UDP_PORT);
+                //    Thread.Sleep(20);
+
+                //}
             }
-            catch (Exception e) { }
-
-
-
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error with passing data to host: {e.Message}");
+            }
         }
 
-        public static Byte[] sendSinglePixelFrame(int numpixels, int pixelon,Byte[] color)
+        /// <summary>
+        /// Turns on a pixel at the pixelon-th position in a frame containing numpixels of LEDs
+        /// </summary>
+        /// <param name="numpixels"></param>
+        /// <param name="pixelon"></param>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        public static Byte[] sendSinglePixelFrame(int numpixels, int pixelon, Byte[] color)
         {
-            Byte[] toreturn = new byte[(numpixels + 2) * 3]; //bytes = pixels + 2 for signaling * 3 for rgb
-            Byte[] clearBytes = { 0x01, 0x00, 0x01 };
-            Byte[] rstBytes = { 0x01, 0x01, 0x00 };
-            //put reset to say new frame
-            for (int i = 0; i < rstBytes.Length; i++)
-                toreturn[i] = rstBytes[i];
-            //set it all to black 
-            for (int i = rstBytes.Length; i < toreturn.Length; i++)
+            // Bytes = pixels + 2 for signaling * 3 for rgb
+            Byte[] toreturn = new byte[(numpixels + 2) * 3]; 
+            
+            // Put reset to say new frame
+            for (int i = 0; i < START.Length; i++)
+                toreturn[i] = START[i];
+            
+            // Set it all to black 
+            for (int i = START.Length; i < toreturn.Length; i++)
                 toreturn[i] = 0;
-            //turn on the single pixel
+            
+            // Turn on the single pixel
             toreturn[pixelon * 3] = color[0];
             toreturn[(pixelon * 3) + 1] = color[1];
             toreturn[(pixelon * 3) + 2] = color[2];
-            //put the clear to tell the thing to display frame.
-            toreturn[toreturn.Length - 3] = clearBytes[0];
-            toreturn[toreturn.Length - 2] = clearBytes[1];
-            toreturn[toreturn.Length - 1] = clearBytes[2];
+
+            // Put the clear to tell the thing to display frame.
+            for (int i = END.Length; i > 0; i--)
+                toreturn[toreturn.Length-i] = END[END.Length-i];
 
             return toreturn;
         }
-
+        
+        /// <summary>
+        /// Sets an entire frame of numpixels to a single color
+        /// </summary>
+        /// <param name="numpixels"></param>
+        /// <param name="color"></param>
+        /// <returns></returns>
         public static Byte[] sendSingleColorFrame(int numpixels, Byte[] color)
         {
-            Byte[] toreturn = new byte[(numpixels+2)*3]; //bytes = pixels + 2 for signaling * 3 for rgb
-            Byte[] clearBytes = { 0x01, 0x00, 0x01 };
-            Byte[] rstBytes = { 0x01, 0x01, 0x00 };
+            //bytes = pixels + 2 for signaling * 3 for rgb
+            Byte[] toreturn = new byte[(numpixels + 2) * 3]; 
+            
             //put reset to say new frame
-            for (int i = 0; i < rstBytes.Length; i++)
-                toreturn[i] = rstBytes[i];
+            for (int i = 0; i < START.Length; i++)
+                toreturn[i] = START[i];
+
             //pack trio of r/g/b for the color 
-            for (int i = rstBytes.Length; i < toreturn.Length;i++)
+            for (int i = START.Length; i < toreturn.Length; i++)
                 toreturn[i] = color[i % 3];
+
             //put the clear to tell the thing to display frame.
-            toreturn[toreturn.Length - 3] = clearBytes[0];
-            toreturn[toreturn.Length - 2] = clearBytes[1];
-            toreturn[toreturn.Length - 1] = clearBytes[2];
+            for (int i = END.Length; i > 0; i--)
+                toreturn[toreturn.Length - i] = END[END.Length - i];
 
             return toreturn;
         }
 
+        /// <summary>
+        /// Returns the next color in the color space
+        /// 
+        /// TODO: Need to split this into multiple methods, to handle different color spaces
+        /// such as HSV
+        /// </summary>
+        /// <param name="currcolor"></param>
+        /// <returns></returns>
         public int nextColor(int currcolor)
         {
             int toreturn = 0;
 
-            byte r = (byte)(currcolor&0x110000 >> 16);
-            byte g = (byte)(currcolor&0x001100 >> 8);
-            byte b = (byte)(currcolor&0x000011);
+            byte r = (byte)(currcolor & 0x110000 >> 16);
+            byte g = (byte)(currcolor & 0x001100 >> 8);
+            byte b = (byte)(currcolor & 0x000011);
 
             //one color is set, increment it. 
             if (r > 0 && r < 255 && b == 0 && g == 0)
@@ -129,12 +171,10 @@ namespace LEDUDPClient
             if (b > 0 && b < 255 && r == 0 && g == 0)
                 b++;
 
-
-
-            return (r<<16)|(g<<8)|(b);
+            return (r << 16) | (g << 8) | (b);
         }
     }
 }
 
-   
+
 
