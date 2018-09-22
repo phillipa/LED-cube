e8fdb0af5bffbd8947aa07be6f389aa080088a92
Matt Brooks <lessthanepsion@gmail.com>
2018-09-22 01:26:35 -0600

    Benchmarks added to measure "effects side" throughput (not teensy throughput)

--- UDPClient/UDPClient/Program.cs
+++ UDPClient/UDPClient/Program.cs
@@ -2,6 +2,7 @@
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using System.Diagnostics;
 
 namespace LEDUDPClient
 {
@@ -20,38 +21,77 @@ namespace LEDUDPClient
     {
         public const string UDP_HOST = "192.168.2.1";
         public const int UDP_PORT = 7777;
-        public const int FRAME_SIZE = 10;
+        public const int FRAME_SIZE = 60;
         public static readonly Byte[] END = { 0x01, 0x00, 0x01 };   // clear
         public static readonly Byte[] START = { 0x01, 0x01, 0x00 }; // reset
 
+        private static void WaitResponse(int delay, bool waitUser)
+        {
+            Thread.Sleep(delay);
+            if (waitUser) Console.ReadKey();
+        }
+
         public static void LEDTestSequence(UdpClient client)
         {
-            int delayBetweenTests = 1000;
+            Console.WriteLine($"UDP Host: {UDP_HOST}");
+            Console.WriteLine($"UDP Port: {UDP_PORT}");
+            Console.WriteLine($"Frame Size: {FRAME_SIZE}");
 
-            // START Test
-            Console.WriteLine("Sending start sequence...");
-            client.Send(START, START.Length);
-            Thread.Sleep(delayBetweenTests);
+            var sw = new Stopwatch();
 
-            // END Test
-            Console.WriteLine("Sending end sequence...");
-            client.Send(END, END.Length);
-            Thread.Sleep(delayBetweenTests);
+            // TODO: Create a delegate that uses WaitResponse as a callback whenever UDPClient.Send is invoked
 
             // Clear the frame
             Console.WriteLine("Wipe the whole frame...");
             Byte[] clearFrame = sendSingleColorFrame(FRAME_SIZE, CRGBColors.BLACK);
             client.Send(clearFrame, clearFrame.Length);
-            Thread.Sleep(delayBetweenTests);
+            WaitResponse(1000, false);
 
             // Purple
             Console.WriteLine("Purple...");
             Byte[] purples = sendSingleColorFrame(FRAME_SIZE, CRGBColors.PURPLE);
             client.Send(purples, purples.Length);
-            Thread.Sleep(delayBetweenTests);
+            WaitResponse(1000, false);
 
+            //Moving pixel
+            Console.WriteLine("Moving pixel...");
+            client.Send(clearFrame, clearFrame.Length); // Clear frame first
+            Byte[] moveSinglePixelFrame;
+            for (int i = 1; i <= FRAME_SIZE; i++)
+            {
+                moveSinglePixelFrame = sendSinglePixelFrame(FRAME_SIZE, i, CRGBColors.RED);
+                client.Send(moveSinglePixelFrame, moveSinglePixelFrame.Length);
+                Thread.Sleep(20);
+            }
+            WaitResponse(1000, false);
+
+            Console.WriteLine("Pixel write test...");
+            client.Send(clearFrame, clearFrame.Length); // Clear frame first
+            Byte[] shiftSinglePixelFrame;
+            int iterations = 5000;
+            int pixelSleep = 0;
+
+            //TODO: Introduce notion of frame sequences
+            int frameSequenceLength = 60;
+            sw.Start();
+            for (int i = 1; i <= (iterations * frameSequenceLength); i++)
+            {
+                if (i % 3 == 0)
+                    shiftSinglePixelFrame = sendSinglePixelFrame(FRAME_SIZE, i % (FRAME_SIZE), CRGBColors.PURPLE);
+                else if (i % 3 == 1)
+                    shiftSinglePixelFrame = sendSinglePixelFrame(FRAME_SIZE, i % (FRAME_SIZE), CRGBColors.ORANGE);
+                else
+                    shiftSinglePixelFrame = sendSinglePixelFrame(FRAME_SIZE, i % (FRAME_SIZE), CRGBColors.AQUA);
+
+                // TODO: UDP frame size may be different from LED frame
+                // LED frames might be large enought to require multiple UDP frames
+                client.Send(shiftSinglePixelFrame, shiftSinglePixelFrame.Length);
+                //Thread.Sleep(pixelSleep);
+            }
+            sw.Stop();
+            Helpers.WriteLEDBenchmarks(FRAME_SIZE, frameSequenceLength, iterations, pixelSleep, sw.Elapsed);
             Console.WriteLine("Done...");
-            Console.ReadKey();
+            WaitResponse(1000, true);
             return;
         }
 
@@ -60,37 +100,26 @@ namespace LEDUDPClient
             var udpClient = new UdpClient(UDP_HOST, UDP_PORT);
 
             try
-            {               
+            {
                 // TODO: Will use key input for controlling color
                 //String input = Console.ReadLine();
-
                 Console.WriteLine("Starting LED test sequence...");
                 LEDTestSequence(udpClient);
-                udpClient.Close();
-                return;
-
-                //for (int i = 0; i < (1000 * FRAME_SIZE); i++)
-                //{
-                //    if (i % 3 == 0)
-                //        tosend = sendSinglePixelFrame(15 * 4, i % (15 * 4), purple);
-                //    else if (i % 3 == 1)
-                //        tosend = sendSinglePixelFrame(15 * 4, i % (15 * 4), orange);
-                //    else if (i % 3 == 2)
-                //        tosend = sendSinglePixelFrame(15 * 4, i % (15 * 4), aqua);
-
-                //    udpClient.Send(tosend, tosend.Length, UDP_HOST, UDP_PORT);
-                //    Thread.Sleep(20);
-
-                //}
             }
             catch (Exception e)
             {
                 Console.WriteLine($"Error with passing data to host: {e.Message}");
             }
+            finally
+            {
+                udpClient.Close();
+            }
         }
 
         /// <summary>
         /// Turns on a pixel at the pixelon-th position in a frame containing numpixels of LEDs
+        /// 
+        /// Pixelon is currently 1-indexed
         /// </summary>
         /// <param name="numpixels"></param>
         /// <param name="pixelon"></param>
@@ -99,16 +128,16 @@ namespace LEDUDPClient
         public static Byte[] sendSinglePixelFrame(int numpixels, int pixelon, Byte[] color)
         {
             // Bytes = pixels + 2 for signaling * 3 for rgb
-            Byte[] toreturn = new byte[(numpixels + 2) * 3]; 
-            
+            Byte[] toreturn = new byte[(numpixels + 2) * 3];
+
             // Put reset to say new frame
             for (int i = 0; i < START.Length; i++)
                 toreturn[i] = START[i];
-            
+
             // Set it all to black 
             for (int i = START.Length; i < toreturn.Length; i++)
                 toreturn[i] = 0;
-            
+
             // Turn on the single pixel
             toreturn[pixelon * 3] = color[0];
             toreturn[(pixelon * 3) + 1] = color[1];
@@ -116,11 +145,11 @@ namespace LEDUDPClient
 
             // Put the clear to tell the thing to display frame.
             for (int i = END.Length; i > 0; i--)
-                toreturn[toreturn.Length-i] = END[END.Length-i];
+                toreturn[toreturn.Length - i] = END[END.Length - i];
 
             return toreturn;
         }
-        
+
         /// <summary>
         /// Sets an entire frame of numpixels to a single color
         /// </summary>
@@ -130,8 +159,8 @@ namespace LEDUDPClient
         public static Byte[] sendSingleColorFrame(int numpixels, Byte[] color)
         {
             //bytes = pixels + 2 for signaling * 3 for rgb
-            Byte[] toreturn = new byte[(numpixels + 2) * 3]; 
-            
+            Byte[] toreturn = new byte[(numpixels + 2) * 3];
+
             //put reset to say new frame
             for (int i = 0; i < START.Length; i++)
                 toreturn[i] = START[i];
